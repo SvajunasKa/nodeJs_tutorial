c85540774439232d017042caf505f684fae742c2
Svajunas <svajunas.kaseta@gmail.com>
2020-02-14 16:31:47 +0200

    init repo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Error</h1>");
+});
+
+app.listen(2000);
